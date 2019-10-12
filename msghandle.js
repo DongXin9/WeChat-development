4c7b8e67fac1c8fdb0b80b9555e3de1878aaff0a
DongXin9 <2627405095@qq.com>
2019-10-12 21:51:32 +0800

    修改 msghandle.js

--- msghandle.js
+++ msghandle.js
@@ -5,53 +5,18 @@ function help(){
     //还可以是以读取文件的形式来返回
     return '你好，这是一个测试号，目前会原样返回用户输入的消息，暂不支持视频类型'
 }
-
 /**
  * @param {object} wxmsg解析XML消息的对象
  * @param {object} retmsg要返回的数据对象
  */
 
 function userMsg(wxmsg,retmsg){
-    //关键词自动回复
-    // if(wxmsg.MsgType == 'text'){
-    //     switch(wxmsg.Content){
-    //         case '帮助':
-    //         case 'help':
-    //         case '?':
-    //             retmsg.msg = help();
-    //             retmsg.msgtype = 'text';
-    //             return formatMsg(retmsg);
-    //             break;
-    //         case 'about':
-    //             retmsg.msgtype = 'text';
-    //             retmsg.msg = '我是这个测试号的开发者，如有问题请发邮件到2333@qq.com';
-    //             return formatMsg(retmsg);
-    //             break;
-    //         default:
-    //             retmsg.msgtype = 'text';
-    //             retmsg.msg = wxmsg.Content;
-    //             return formatMsg(retmsg);
-    //             break;
-    //     }
-    // }
-    //处理其他消息
-    // switch(wxmsg.MsgType){
-    //     case 'image':
-    //     case 'voice':
-    //         retmsg.msgtype = wxmsg.MsgType;
-    //         retmsg.msg = wxmsg.MediaID;
-    //         return formatMsg(retmsg);
-    //         break;
-    //     default:
-    //         //retmsg.msgtype类型为空
-    //         //格式化数据会返回default处的数据
-    //         //提示用户改类型不被支持
-    //         return formatMsg(retmsg);
-    // }
     /*
         检测是否为文本消息，如果是文本消息则先要检测是不是支持的关键词回复。
     */
+    //关键字自动回复
     if (wxmsg.MsgType == 'text') {
+
         if (wxmsg.Content == '帮助' || wxmsg.Content == 'help' || wxmsg.Content == '?' || wxmsg.Content == '？') {
             retmsg.msg = help();
             retmsg.msgtype = 'text';
@@ -60,12 +25,15 @@ function userMsg(wxmsg,retmsg){
             retmsg.msg = '你好，你可以输入一些关键字测试消息回复，输入help/?获取帮助';
             retmsg.msgtype = 'text';
             return formatMsg(retmsg);
-
         } else if(wxmsg.Content == 'who'){
             retmsg.msg = '----------开发者信息----------\n姓名：董心\n学号：2017011950\n'
             retmsg.msgtype = 'text';
             return formatMsg(retmsg);
-        }else {
+        } else if(wxmsg.Content == 'about'){
+            retmsg.msg = '我是这个测试号的开发者，如有问题请发邮件到23333@qq.com'
+            retmsg.msgtype = 'text';
+            return formatMsg(retmsg);
+        } else {
             retmsg.msg = wxmsg.Content;
             retmsg.msgtype = wxmsg.MsgType;
             return formatMsg(retmsg);
@@ -78,6 +46,9 @@ function userMsg(wxmsg,retmsg){
                 retmsg.msgtype = wxmsg.MsgType;
                 break;
             default:
+                //retmsg.msgtype类型为空
+                //格式化数据会返回default处的数据
+                //提示用户改类型不被支持
                 retmsg.msg = '不支持的类型';
         }
 
